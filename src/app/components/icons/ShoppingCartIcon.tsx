52b77c9a724d502bb3fa465b27ffcd3d540a8667
ceejaymar <martinezcj2@gmail.com>
2025-07-10 01:05:52 -0400

    Add navbar

---
+++ src/app/components/icons/ShoppingCartIcon.tsx
@@ -0,0 +1,26 @@
+"use client";
+import {
+  ShoppingCartIcon as ShoppingCart,
+  type IconWeight,
+} from "@phosphor-icons/react";
+
+type ShoppingCartIconProps = {
+  color?: string;
+  size?: number;
+  weight?: IconWeight;
+};
+
+export default function ShoppingCartIcon({
+  color,
+  size,
+  weight,
+}: ShoppingCartIconProps) {
+  return (
+    <ShoppingCart
+      aria-label="View shopping cart"
+      size={size}
+      weight={weight}
+      color={color}
+    />
+  );
+}
---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+import ShoppingCartIcon from "../icons/ShoppingCartIcon";
+import classes from "./Navbar.module.css";
+
+const links = [
+  // { name: 'Home', path: '/' },
+  { name: "lookbook", path: "/lookbook" },
+  { name: "about", path: "/about" },
+  { name: "contact", path: "/contact" },
+  { name: "shop", path: "/shop" },
+];
+
+export default function Navbar() {
+  return (
+    <nav data-testid="navigation" className={classes.nav}>
+      <Link href="/">
+        <span className={classes.brand}>Batéy</span>
+      </Link>
+      <ul className={classes.navList}>
+        {links.map((link) => (
+          <li key={link.name}>
+            <Link href={`${link.path}`} className={classes.navLink}>
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className={classes.cartContainer}>
+        <Link href="cart">
+          <ShoppingCartIcon
+            aria-label="View shopping cart"
+            size={24}
+            weight="regular"
+            color="blue"
+          />
+        </Link>
+      </div>
+    </nav>
+  );
+}
